3b534fdf65d40e4f34b3d6b282d3bdc4b230d37c
Zaki Indra Yudhistira <55521899+zaki-indra@users.noreply.github.com>
2026-04-23 09:47:46 +0700

    Debug printer
    Signed-off-by: Zaki Indra Yudhistira <55521899+zaki-indra@users.noreply.github.com>

--- server/debug.c
+++ server/debug.c
@@ -5,11 +5,29 @@
 #include <stdio.h>
 #include <string.h>
 
-void print_dns(const DnsMessage *dns) {
+void print_dns(const DnsMessage* dns)
+{
 #ifdef DEBUG
     printf("DNS message:\n");
     printf("  ID: %hu\n", dns->id);
-    char flags[15];
+    char flags[15] = "";
+#ifdef _WIN32
+    if (flags_get_qr(dns->flags) == QR_RESPONSE) {
+        strcat_s(flags, sizeof(flags), "qr ");
+    }
+    if (flags_get_aa(dns->flags) == AA_YES) {
+        strcat_s(flags, sizeof(flags), "aa ");
+    }
+    if (flags_get_tc(dns->flags) == TC_YES) {
+        strcat_s(flags, sizeof(flags), "tc ");
+    }
+    if (flags_get_rd(dns->flags) == RD_YES) {
+        strcat_s(flags, sizeof(flags), "rd ");
+    }
+    if (flags_get_ra(dns->flags) == RA_YES) {
+        strcat_s(flags, sizeof(flags), "ra ");
+    }
+#else
     if (flags_get_qr(dns->flags) == QR_RESPONSE) {
         strcat(flags, "qr ");
     }
@@ -25,17 +43,33 @@ void print_dns(const DnsMessage *dns) {
     if (flags_get_ra(dns->flags) == RA_YES) {
         strcat(flags, "ra ");
     }
-
+#endif
     printf("  flags: %s\n", flags);
     printf("  QDCOUNT: %hu\n", dns->qdcount);
     printf("  ARCOUNT: %hu\n", dns->arcount);
 
     printf("  Questions:\n");
     for (int i = 0; i < dns->qdcount; i++) {
-        printf("    %d:\n", i);
+        printf("    - %d:\n", i);
         printf("      QNAME: %s\n", dns->questions[i].qname);
         printf("      QTYPE: %hu\n", dns->questions[i].qtype);
         printf("      QCLASS: %hu\n", dns->questions[i].qclass);
     }
+    if (dns->arcount > 0) {
+        printf("  Additional:\n");
+    }
+    // for (int i = 0; i < dns->arcount; i++) {
+    //     printf("    - %d:\n", i);
+    //     printf("      NAME: %s\n", dns->additional[i].name);
+    //     printf("      TYPE: %hu\n", dns->additional[i].type);
+    //     printf("      CLASS: %hu\n", dns->additional[i].class);
+    //     printf("      TTL: %u\n", dns->additional[i].ttl);
+    //     printf("      RDLENGTH: %hu\n", dns->additional[i].rdlength);
+    //     printf("      RDATA: ");
+    //     for (int j = 0; j < dns->additional[i].rdlength; j++) {
+    //         printf("%02x ", dns->additional[i].rdata[j]);
+    //     }
+    //     printf("\n");
+    // }
 #endif
 }
